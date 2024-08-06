c47ebddc9d65617cc91588e4cfaec234bb7e4787
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2024-08-06 14:06:14 +0300

    email stuff

--- sometestapi/settings.py
+++ sometestapi/settings.py
@@ -28,7 +28,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 # Application definition
 
 INSTALLED_APPS = [
